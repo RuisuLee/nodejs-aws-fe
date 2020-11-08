02ab9cbc6f41d1b94435c16a17b43ceced03f147
Aleksandra Zaitceva <aleksandra_zaitceva@epam.com>
2020-11-08 19:43:19 +0400

    add task 4

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -10,6 +10,8 @@ export type Product = {
 
 export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
-  description: Yup.string(),
+  description: Yup.string().required(),
+  img: Yup.string().required(),
   price: Yup.number().required(),
+  count: Yup.number().required()
 });
