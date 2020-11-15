08f4e1a6f2479762f38fdc344d89922620d0061b
Aleksandra Zaitceva <aleksandra_zaitceva@epam.com>
2020-11-15 22:16:37 +0400

    add task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ssbohnuykl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://mxwvyv7tnl.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
