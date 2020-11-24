a7548afc5f8b193d20ff41a981757a9c6648c548
Aleksandra Zaitceva <aleksandra_zaitceva@epam.com>
2020-11-24 13:55:58 +0400

    change urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ssbohnuykl.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://mxwvyv7tnl.execute-api.eu-west-1.amazonaws.com/dev'
+  import: 'https://irrnw5oj9h.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://utotrl1311.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
